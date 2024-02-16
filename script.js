33f835aab378e0e07706272a72072eb563c05bc2
Melissa Martins <melmartins.pro@gmail.com>
2024-02-16 15:23:16 -0300

    foto adicionada

---
+++ script.js
@@ -0,0 +1,11 @@
+let t = window.document.getElementById('twitter')
+let i = window.document.getElementById('instagram')
+t.addEventListener('click', twitter)
+i.addEventListener('click', insta)
+
+function twitter(){
+    t.innerText = "I dont have a twitter :/"
+}
+
+function insta(){
+ i.innerText = "I don't wanna give you this"}
